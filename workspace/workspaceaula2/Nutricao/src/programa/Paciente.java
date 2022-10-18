d280e180dce0e9e69e0c677f4ec7a07ca94cc594
Aluno <joaopedroangeloni@gmail.com>
2022-10-18 08:34:52 -0300

    SENAI FPOO

--- workspace/workspaceaula2/Nutricao/src/programa/Paciente.java
+++ workspace/workspaceaula2/Nutricao/src/programa/Paciente.java
@@ -3,6 +3,9 @@ package programa;
 public class Paciente {
 	String nome;
 	float peso, altura;
+	float mediapeso = 1;
+	float mediaaltura = 1;
+	float mediaimc = 1;
 	
 	Paciente(){}
 	Paciente(String nomec, float pesoc, float alturac){
@@ -15,15 +18,15 @@ public class Paciente {
 		return peso/(altura*altura);
 	}
 	String diagnostico(){
-		if(peso/(altura*altura)<16f)return "Subpeso Severo";
-		else if(peso/(altura*altura)<=19.9f)return "Subpeso";
-		else if(peso/(altura*altura)<=24.9f)return "Normal";
-		else if(peso/(altura*altura)<=29.9f)return "Sobrepeso";
-		else if(peso/(altura*altura)<39.9f)return "Obeso";
+		if(imc()<16f)return "Subpeso Severo";
+		else if(imc()<=19.9f)return "Subpeso";
+		else if(imc()<=24.9f)return "Normal";
+		else if(imc()<=29.9f)return "Sobrepeso";
+		else if(imc()<39.9f)return "Obeso";
 		else return "Obeso Mórbido";
 	}
 	public String toString() {
-		return String.format("[%s] [%.2f] [%.2f] [%.2f] [%s]\n",nome,peso,altura,imc(),diagnostico());
+		return String.format("%s\t\t%.2f\t%.2f\t%.2f\t%s", nome, peso, altura, imc(), diagnostico());
 	}
 }
 
